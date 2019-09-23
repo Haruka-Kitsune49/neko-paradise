447ee96f6632317a7d90bc7e87f6c377fe119323
Haruka-Kitsune49 <55716018+Haruka-Kitsune49@users.noreply.github.com>
2019-09-23 22:26:28 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("/")
+
+bot.on('ready', function() {
+    bot.user.setGame('Command: /help');
+    console.log("Connected");
+});
+
+bot.login('NjIyNzA1ODg4MTAwMDg5ODY3.XYj5sw.gN7YTN-p8fTdBn_Wx4YVaKD1gQM');
+
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage('Liste des commandes: \n -/help');
+    }
+
+    if (message.content === "Heyy"){
+        message.reply("Coucou hi hi ^^");
+        console.log("Commande Salut effectué");
+    }
+});
